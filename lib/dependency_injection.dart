58eb91f9361c8f8119231a576581e997e27790bd
Entruna <im.tired94@hotmail.com>
2025-07-15 13:30:10 +0200

    add ui logic

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -4,13 +4,20 @@ import 'package:github_repository_finder/data/remote/github_remote_data_source.d
 import 'package:github_repository_finder/data/remote/github_remote_data_source_impl.dart';
 import 'package:github_repository_finder/data/repository/github_repository_impl.dart';
 import 'package:github_repository_finder/domain/repository/github_repository.dart';
+import 'package:github_repository_finder/presentation/bloc/search_cubit.dart';
+import 'package:logger/logger.dart';
 
 final getIt = GetIt.instance;
 
 ///[setupInjection] sets dependency injection
 void setupInjection() {
+  getIt.registerLazySingleton<Logger>(() => Logger());
+
   ///Data layer injections
   getIt.registerLazySingleton<Dio>(() => Dio());
   getIt.registerLazySingleton<GitHubRemoteDataSource>(() => GitHubRemoteDataSourceImpl(dio: getIt<Dio>()));
   getIt.registerLazySingleton<GitHubRepository>(() => GitHubRepositoryImpl(remoteDataSource: getIt<GitHubRemoteDataSource>()));
+
+  ///Presentation layer injections
+  getIt.registerLazySingleton<SearchCubit>(() => SearchCubit(gitHubRepository: getIt<GitHubRepository>(), logger: getIt<Logger>()));
 }
---
+++ lib/presentation/bloc/search_state.dart
@@ -0,0 +1,17 @@
+import 'package:github_repository_finder/domain/models/repository_domain_model.dart';
+
+abstract class SearchState {}
+
+class SearchInitial extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchLoaded extends SearchState {
+  final List<RepositoryDomainModel> repos;
+  SearchLoaded(this.repos);
+}
+
+class SearchError extends SearchState {
+  final String message;
+  SearchError(this.message);
+}
